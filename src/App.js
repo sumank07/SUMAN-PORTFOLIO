31a3739db2bd78762e721d485d56a09abdb88e8b
suman katuwal <suman.katuwal@aadimcollege.edu.np>
2024-12-21 13:58:12 +0545

    change

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import {React,useEffect }  from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "./components/Navbar";
+import Home from "./components/Home";
 import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
-import Skill from "./components/Skills";
-import Home from "./components/Home";
+import Skills from "./components/Skills";
 
 function App() {
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
+  const toggleBtn = () => {
+    setIsDarkMode((prev) => !prev);
+  };
+
   useEffect(() => {
     document.documentElement.style.scrollBehavior = "smooth";
-  }, []);
+    document.body.style.backgroundColor = isDarkMode ? "black" : "white";
+    document.body.style.color = isDarkMode ? "white" : "black";
+  }, [isDarkMode]);
 
   return (
-    <div className="dark:bg-black dark:text-white">
-      <Navbar/>
+    <div>
+      <Navbar  toggleBtn={toggleBtn} isDarkMode={isDarkMode} />
       <Home />
       <About />
-      <Skill />
+      <Skills />
       <Contact />
       <Footer />
     </div>
   );
-};
+}
 
 export default App;
